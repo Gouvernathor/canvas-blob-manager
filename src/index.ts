3bf103cfd237498b9f246c4afa32ee94b6a178ec
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-07-27 02:11:24 +0200

    Initial implem

---
+++ src/index.ts
@@ -0,0 +1,85 @@
+/**
+ * This can be subclassed, although it is more advised to instantiate this class and use one instance.
+ */
+export default class BlobManager {
+    /**
+     * @param blobMimes Mime types for the image blobs, by decreasing preference order.
+     */
+    constructor(
+        private getCanvas: () => HTMLCanvasElement,
+        private blobMimes: string[] = [ "image/webp", "image/png" ],
+    ) {}
+
+    private async getBlobs() {
+        const blobs: Record<string, Blob> = {};
+
+        const canvas = this.getCanvas();
+        await Promise.allSettled(this.blobMimes.map(mime =>
+            new Promise<void>(resolve => {
+                canvas.toBlob(blob => {
+                    if (blob === null) {
+                        console.warn(`Failed to extract data as ${mime} from canvas`);
+                    } else {
+                        blobs[mime] = blob;
+                    }
+                    resolve();
+                }, mime, 1.);
+            })
+        ));
+
+        return blobs;
+    }
+
+    private url: string = "";
+    /**
+     * Downloads the canvas as an image file, through the browser.
+     */
+    public async downloadCanvas(filenameNoExtension = "image"): Promise<void> {
+        const blobs = await this.getBlobs();
+        const mime = this.blobMimes.find(mime => blobs[mime] !== undefined);
+        if (mime === undefined) {
+            console.error("No blobs available for download");
+            return;
+        }
+
+        const blob = blobs[mime]!;
+        const a = document.createElement("a");
+        a.download = `${filenameNoExtension}.${mime.split("/")[1]}`; // though most browsers fix the extension automatically
+        if (this.url) {
+            URL.revokeObjectURL(this.url);
+        }
+        a.href = this.url = URL.createObjectURL(blob);
+        a.click();
+    }
+
+    /**
+     * Copies the canvas's content to the clipboard as an image.
+     */
+    public async copyCanvas(): Promise<void> {
+        const blobs = await this.getBlobs();
+
+        for (const mime of this.blobMimes) {
+            if (ClipboardItem.supports && !ClipboardItem.supports(mime)) {
+                console.warn(`ClipboardItem does not support ${mime}`);
+                continue;
+            }
+
+            const blob = blobs[mime];
+            if (!blob) {
+                continue;
+            }
+
+            try {
+                await navigator.clipboard.write([
+                    new ClipboardItem({ [blob.type]: blob }),
+                ]);
+            } catch (e) {
+                console.error(`Failed to copy canvas to clipboard as ${mime}: ${e}`);
+                continue;
+            }
+            console.log(`Copied canvas as ${mime} to clipboard`);
+            return;
+        }
+        console.error("No blobs to copy");
+    }
+}
